9c4ee29aaac49c3ad1e700523430b21f1bbfa381
= <=>
2022-04-23 21:31:19 -0500

    eliminacion

--- taller corte 2 web1-main/fundamentos/src/App.js
+++ taller corte 2 web1-main/fundamentos/src/App.js
@@ -1,10 +1,4 @@
-import logo from './logo.svg';
-import Parrafo from './components/Parrafo';
-import Variables from './components/Variables';
-import Contador from './components/Contador';
-import Eventos from './components/Eventos';
-import Listas from './components/Listas';
-import PeticionApi from './components/PeticionApi';
+
 import Formulario from './components/Formulario';
 import './App.css';
 
